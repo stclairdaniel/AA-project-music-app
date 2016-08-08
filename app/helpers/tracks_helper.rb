f71163e803178bd3d07318c4b9b7bbbe5131aa27
Daniel St. Clair <stclair.daniel@gmail.com>
2016-08-07 17:36:05 -0700

    added lyrics display with track helper

--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -1,2 +1,13 @@
 module TracksHelper
+  include ERB::Util
+
+  def ugly_lyrics(lyrics)
+    lyrics_lines = lyrics.split("\n")
+    res = ""
+    lyrics_lines.each do |line|
+      res << "<pre>&#9835; #{h(line)}</pre>"
+    end
+    res.html_safe
+  end
+
 end
